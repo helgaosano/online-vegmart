75b0f9ea3b87c07c0a0629f6c8ac2a886eb33b52
tania <taniaosano15@gmail.com>
2022-12-22 00:47:47 +0300

    working on navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+
+import React from "react";
+import { NavLink} from 'react-router-dom'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
+function Navbar(){
+
+    return(
+        <div className="navbar">
+            
+           <div className="organics">
+            <h1>VegMart</h1>
+            </div> 
+            
+            <div className="nav-components">    
+            <ul>
+                <li>
+                    <NavLink to="/">Home</NavLink>
+                </li>
+                <li>
+                    <AnchorLink to="/components/about">About</AnchorLink>
+                </li>
+               
+                <li>
+                    <NavLink to="/components/login">Login</NavLink>
+                </li>
+            </ul>  
+            </div>
+            
+        </div>
+    )
+
+}
+
+export default Navbar;
